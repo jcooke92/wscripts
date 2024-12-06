3b2c15a8e909fdfcee0724d2da99940a3c5a692f
Jonathan Cooke <jonathan.a.cooke@outlook.com>
2024-12-06 11:20:52 -0600

    init commit

---
+++ gifconvert.bat
@@ -0,0 +1,9 @@
+@echo off
+if "%~1"=="" (
+    echo Usage: gifconvert input.mp4 scale
+    exit /b
+)
+set input=%1
+set scale=%2
+
+ffmpeg -i "%input%" -vf "fps=30,scale=%scale%:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 1 "%input%.gif"
